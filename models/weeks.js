dd3b7187cbbc876422cb68ef33a24035760123c9
Ayomide Ojelade <ojeladea@outlook.com>
2022-01-18 12:10:40 +0000

    http requests and model function

---
+++ models/weeks.js
@@ -0,0 +1,21 @@
+// import query from "../db/index.js";
+
+export async function getAllWeeks() {
+  const data = await query(`SELECT * FROM weeks;`);
+  return data.rows;
+}
+
+export async function getWeekById(id) {
+  const data = await query(`SELECT * FROM weeks WHERE id = $1;`, [id]);
+  return data.rows;
+}
+export async function getDayById(id) {
+  const data = await query(`SELECT * FROM days WHERE id = $1;`, [id]);
+  return data.rows;
+}
+export async function createResource(id) {
+  const data = await query(`INSERT INTO resources FROM topics WHERE id=$1`, [
+    id,
+  ]);
+  return data.rows;
+}
