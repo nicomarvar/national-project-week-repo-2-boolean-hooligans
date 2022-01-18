2e949d5d0482c1a7ed3deed45224ea9067e1ced8
nicomarvar <nicomarvar@gmail.com>
2022-01-18 10:02:32 +0000

    app.js routes folder es6 configured

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+import Router from './routes/index';
 
-var app = express();
+export const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +36,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
