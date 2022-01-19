adc5af9c9aae85b0ba457723b507c70e238fde7e
nicomarvar <nicomarvar@gmail.com>
2022-01-19 13:38:39 +0000

    typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import createError from "http-errors";
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import cors from "cors";
+import cors from "cors"
 
 import Router from "./routes/index.js";
 
@@ -10,13 +10,13 @@ const app = express();
 // const PORT = 3000;
 
 // view engine setup
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", Router);
-app.use(cors())
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
