1172e0cd30c035612bbb95972686e9cb594c231e
nicomarvar <nicomarvar@gmail.com>
2022-01-19 11:42:04 +0000

    routes working

--- models/weeks.js
+++ models/weeks.js
@@ -6,19 +6,20 @@ export async function getAllWeeks() {
 }
 
 export async function getWeekById(id) {
-  const data = await query(`SELECT * FROM weeks WHERE weekid = $1;`[id]);
+  const data = await query(`SELECT * FROM weeks WHERE Weekid = $1;`,[id]);
   return data.rows;
 }
+
 export async function getDayById(id) {
-  const data = await query(`SELECT * FROM days WHERE Daysid = $1;`[id]);
+  const data = await query(`SELECT * FROM days WHERE Daysid = $1;`,[id]);
   return data.rows;
 }
 export async function getResourceById(id) {
-  const data = await query(`SELECT * FROM days WHERE Daysid = $1;`[id]);
+  const data = await query(`SELECT * FROM days WHERE Daysid = $1;`,[id]);
   return data.rows;
 }
 
 export async function createResource(id) {
-  const data = await query(`SELECT * FROM Resources WHERE Topicid = $1;`[id]);
+  const data = await query(`SELECT * FROM Resources WHERE Topicid = $1;`,[id]);
   return data.rows;
 }
