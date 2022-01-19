c19fc2f42f1d516aff74ca48a9acae1d42068cab
nicomarvar <nicomarvar@gmail.com>
2022-01-19 13:19:07 +0000

    typo

--- app.js
+++ app.js
@@ -10,14 +10,13 @@ const app = express();
 // const PORT = 3000;
 
 // view engine setup
-app.use(cors)
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", Router);
-
+app.use(cors)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
