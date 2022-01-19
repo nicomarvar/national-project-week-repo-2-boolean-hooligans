bd88bd4df23ee7a57a936eab382dcea1a5795ee6
Ayomide Ojelade <ojeladea@outlook.com>
2022-01-19 10:00:12 +0000

    weekid

--- models/weeks.js
+++ models/weeks.js
@@ -6,7 +6,7 @@ export async function getAllWeeks() {
 }
 
 export async function getWeekById(id) {
-  const data = await query(`SELECT * FROM weeks WHERE id = $1;`[id]);
+  const data = await query(`SELECT * FROM weeks WHERE Weekid = $1;`[id]);
   return data.rows;
 }
 export async function getDayById(id) {
