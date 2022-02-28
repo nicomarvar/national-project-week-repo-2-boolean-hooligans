1dd00efa00a4c690ee6f6c070c696b8a895fb0de
nicomarvar <nicomarvar@gmail.com>
2022-02-28 11:21:57 +0000

    CORS

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const app = express();
 // const PORT = 3000;
 
 // view engine setup
-app.use(cors());
+app.use(cors(
+    {
+      origin: "*",
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+      credentials: true, mode: "no-cors",
+  }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
